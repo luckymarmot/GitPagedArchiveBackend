0b5f1e3cba9a77d293804abec6138ad5b3d863b1
Matthaus Woolard <matthaus.woolard@gmail.com>
2017-01-18 17:22:55 +0000

    compile to temp dir

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from Cython.Distutils import Extension
+
+ex = Extension('*', ['./src/*.pyx'], cython_c_in_temp=True)
+
+extensions = [ex,]
 
 setup(
-    ext_modules=cythonize("backend.pyx")
+    ext_modules=cythonize(extensions)
 )
