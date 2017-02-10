d9d9b5d7ca9d577fc18568c80a0b9d0a9b77cc1c
Matthaus Woolard <matthaus.woolard@gmail.com>
2017-02-10 09:53:01 +0000

    complete setup script

---
+++ paged_archive/__init__.py
@@ -0,0 +1,4 @@
+from paged_archive._archive import ArchiveRepository, PagedArchive
+
+
+__all__ = ['ArchiveRepository', 'PagedArchive']
--- setup.py
+++ setup.py
@@ -1,30 +1,70 @@
-from distutils.command import build_clib, build_ext
+import setuptools
 from distutils.core import setup
-from Cython.Build import cythonize
-from Cython.Distutils import Extension
-from pygit2._build import get_libgit2_paths
 
-libgit2_bin, libgit2_include, libgit2_lib = get_libgit2_paths()
 
-ex_archive = Extension(
-    name='paged_archive',
-    sources=[
-             './src/paged_archive.pyx',
-             './src/gitbackend.c',
-             './src/ArchiveLib/archive/Archive.c',
-             './src/ArchiveLib/archive/ArchivePage.c',
-             './src/ArchiveLib/archive/HashIndex.c',
-             './src/ArchiveLib/archive/HashIndexPack.c'
-    ],
-    cython_c_in_temp=True,
-    libraries=['git2'],
-    include_dirs=[libgit2_include, './src/ArchiveLib/archive'],
-    library_dirs=[libgit2_lib]
-)
+def ext_modules():
+    from Cython.Build import cythonize
+    from Cython.Distutils import Extension
+    from pygit2._build import get_libgit2_paths
 
-extensions = [ex_archive,]
+    libgit2_bin, libgit2_include, libgit2_lib = get_libgit2_paths()
+
+    ex_archive = Extension(
+        'paged_archive._archive',
+        sources=[
+            './src/_archive.pyx',
+            './src/gitbackend.c',
+            './src/ArchiveLib/archive/Archive.c',
+            './src/ArchiveLib/archive/ArchivePage.c',
+            './src/ArchiveLib/archive/HashIndex.c',
+            './src/ArchiveLib/archive/HashIndexPack.c'
+        ],
+        cython_c_in_temp=True,
+        libraries=['git2'],
+        include_dirs=[libgit2_include, './src/ArchiveLib/archive'],
+        library_dirs=[libgit2_lib]
+    )
+
+    extensions = [ex_archive, ]
+
+    return cythonize(extensions)
+
+
+class lazy_cythonize(list):
+    def __init__(self, callback):
+        self._list, self.callback = None, callback
+
+    def c_list(self):
+        if self._list is None: self._list = self.callback()
+        return self._list
+
+    def __iter__(self):
+        for e in self.c_list(): yield e
+
+    def __getitem__(self, ii):
+        return self.c_list()[ii]
+
+    def __len__(self):
+        return len(self.c_list())
+
 
 setup(
     name="paged_archive",
-    ext_modules=cythonize(extensions),
+    ext_modules=lazy_cythonize(ext_modules),
+    dependency_links=[
+        'git+https://github.com/libgit2/pygit2.git@master'
+    ],
+
+    setup_requires=[
+        'pygit2==0.25.0',
+        'Cython==0.25.2'],
+    install_requires=[
+        'pygit2==0.25.0',
+        'Cython==0.25.2'
+    ],
+    version='0.0.dev0',
+    url='https://github.com/luckymarmot/GitPagedArchiveBackend',
+    maintainer='Matthaus Woolard',
+    maintainer_email="matt@paw.cloud",
+    packages=['paged_archive']
 )
